60059af39f012e991357d5cc23584353946ee103
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-24 23:13:48 +0530

    basic java

---
+++ basic.java
@@ -0,0 +1,15 @@
+
+public class basic {
+
+    public static void main(String[] args) {
+        String s = "hi";
+        int a = 10;
+        double b = 5.5;
+        System.out.println(s);
+        System.out.println(a);
+        System.out.println(b);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
