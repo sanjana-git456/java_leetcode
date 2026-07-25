0558171de11a95f9617adad859ab773572c163b3
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-25 23:56:44 +0530

    palindrome

---
+++ Palindrome.java
@@ -0,0 +1,29 @@
+
+public class Palindrome {
+
+    public void pal(char[] word) {
+        int left = 0;
+        int right = word.length - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(word[left])) {
+                left += 1;
+            }
+            while (left < right && !Character.isLetterOrDigit(word[right])) {
+                right -= 1;
+            }
+            if (Character.toLowerCase(word[left]) != Character.toLowerCase(word[right])) {
+                System.out.println("False");
+                return;
+            }
+            left += 1;
+            right -= 1;
+        }
+        System.out.println("True");
+    }
+
+    public static void main(String[] args) {
+        Palindrome p = new Palindrome();
+        char[] w = "A man, a plan, a canal: Panama".toCharArray();
+        p.pal(w);
+    }
+}
