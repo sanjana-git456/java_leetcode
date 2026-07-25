1874a4366620d150ee3a512db7b83d67e0b1d6bc
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-25 23:38:51 +0530

    anagrams

---
+++ Anagram.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class Anagram {
+
+    public void anagr(char[] s, char[] t) {
+        Arrays.sort(s);
+        Arrays.sort(t);
+        if (Arrays.equals(s, t)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+
+    public static void main(String[] args) {
+        Anagram a = new Anagram();
+        char[] arr1 = "anagram".toCharArray();
+        char[] arr2 = "angrama".toCharArray();
+        a.anagr(arr1, arr2);
+    }
+}
