40ab8c0376bf16b1c7bbc1e3393bed89b307ee30
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-29 02:31:29 +0530

    corrected longest substring sum

--- Kadane.java
+++ Kadane.java
@@ -4,7 +4,7 @@ public class Kadane {
     public int largest(int[] nums) {
         int m = nums[0];
         int maxsofar = nums[0];
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 1; i < nums.length; i++) {
             m = Math.max(nums[i], m + nums[i]);
             if (m > maxsofar) {
                 maxsofar = m;
@@ -15,7 +15,7 @@ public class Kadane {
 
     public static void main(String[] args) {
         Kadane k = new Kadane();
-        int[] nums = {-3, -1, -2};
+        int[] nums = {3, -1, -1};
         System.out.println(k.largest(nums));
     }
 }
