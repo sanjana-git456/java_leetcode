e3728a34153ab0810bd61be4be6c5a212c05ca24
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-29 23:56:40 +0530

    merge two sorted linked list

--- Merge.java
+++ Merge.java
@@ -36,4 +36,23 @@ public class Merge {
 
         return dummy.next;
     }
+
+    public static void main(String[] args) {
+        Merge m = new Merge();
+        Merge.ListNode a1 = m.new ListNode(1);
+        Merge.ListNode a2 = m.new ListNode(2);
+        Merge.ListNode a3 = m.new ListNode(4);
+        a1.next = a2;
+        a2.next = a3;
+        Merge.ListNode b1 = m.new ListNode(1);
+        Merge.ListNode b2 = m.new ListNode(3);
+        Merge.ListNode b3 = m.new ListNode(4);
+        b1.next = b2;
+        b2.next = b3;
+        ListNode result = m.mer(a1, b1);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
 }
