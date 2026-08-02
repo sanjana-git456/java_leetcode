6c9b09ff0e113e65276510333116c4cc04c0c031
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-02 23:23:34 +0530

    palindrome

--- Palindrome.java
+++ Palindrome.java
@@ -4,15 +4,15 @@ public class Palindrome {
     public boolean pal(char[] x) {
         int left = 0;
         int right = x.length - 1;
-        for (int i = 0; i < x.length; i++) {
-            if (!Character.isLetterOrDigit(x[left])) {
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(x[left])) {
                 left++;
-            } else if (!Character.isLetterOrDigit(x[right])) {
+            }
+            while (left < right && !Character.isLetterOrDigit(x[right])) {
                 right--;
-            } else {
-                if (Character.toLowerCase(x[left]) != Character.toLowerCase(x[right])) {
-                    return false;
-                }
+            }
+            if (Character.toLowerCase(x[left]) != Character.toLowerCase(x[right])) {
+                return false;
             }
             left++;
             right--;
