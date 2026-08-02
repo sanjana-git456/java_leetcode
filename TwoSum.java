81ffc05d629a932e369a9deef7657c5d8cf70a77
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-02 19:56:58 +0530

    twosum hash method

--- TwoSum.java
+++ TwoSum.java
@@ -3,16 +3,21 @@ import java.util.*;
 
 public class TwoSum {
 
-    public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
+    public void two(int[] x, int t) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int a = target - nums[i];
+        for (int i = 0; i < x.length; i++) {
+            int a = t - x[i];
             if (map.containsKey(a)) {
                 System.out.println(map.get(a) + " " + i);
             }
-            map.put(nums[i], i);
+            map.put(x[i], i);
         }
+    }
+
+    public static void main(String[] args) {
+        TwoSum ts = new TwoSum();
+        int[] arr = {2, 7, 11, 15};
+        int t = 9;
+        ts.two(arr, t);
     }
 }
